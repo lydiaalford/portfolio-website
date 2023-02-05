d8f95d3ab8790646bec1fe16a49c157bc3e96e29
lydiaalford <121990286+lydiaalford@users.noreply.github.com>
2023-02-04 22:04:02 -0600

    include JS interaction with html pages

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,16 @@
 console.log("Hi. I'm glad you popped in!")
+
+function menuToggle() {
+    var x = document.getElementById('myNavtoggle');
+    x.className += ' responsive';
+  }
+
+  function menuToggle() {
+    var x = document.getElementById('myNavtoggle');
+    if (x.className === 'navtoggle') {
+      x.className += ' responsive';
+    } else {
+      x.className = 'navtoggle';
+    }
+  }
+
